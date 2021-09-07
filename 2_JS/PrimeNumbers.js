374284755d26b6cbb804379216040554803f788a
Adotac <otacool89@gmail.com>
2021-09-07 23:44:46 +0800

    Auto stash before checking out "HEAD"

--- 2_JS/PrimeNumbers.js
+++ 2_JS/PrimeNumbers.js
@@ -1,16 +1,43 @@
-var num = 6;
+/*
+Before running this script on terminals/console.
+Make sure you have the "node_modules" on the same folder of the script.
 
+You can also install the module on the same directory with this command:
+
+    npm install prompt
+*/
+
+const prompt = require('prompt');
+prompt.start();
+
+var num = { 
+    description: 'Input a number',
+    name: 'num', 
+    type: 'integer',
+    warning: 'Invalid Input!' 
+};
 var primeflag = false;
 var factorsCtr = 0;
-for(var i = 1; i < num; i++){
-    if(num % i == 0 && num != 1)
-        factorsCtr++; //counts the number of factors the number have
 
-    if(factorsCtr > 1)
-        break;
+prompt.get(num, function(err, result) {
+    if (err) { return onErr(err); }  
+
+    for(var i = 1; i < result.num; i++){
+        if(result.num % i == 0 && result.num != 1)
+            factorsCtr++; //counts the number of factors the number have
+    
+        if(factorsCtr > 1)
+            break;
+    }
+    
+    if(factorsCtr == 1)
+        primeflag = true;
+    
+    console.log(`\n${result.num} is a prime number?\n${primeflag}`);
+    
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
 }
-
-if(factorsCtr == 1)
-    primeflag = true;
-
-console.log(`${num} is a prime number?\n${primeflag}`);
